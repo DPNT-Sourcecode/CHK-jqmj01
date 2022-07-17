792801439e8f533acf0d6007f42b249da353d69a
sbx_user1051 <sbx_user1051@169.254.1.109>
2022-07-17 17:03:43 +0000

    Sun Jul 17 17:03:43 UTC 2022

--- src/checkout/basket_handler.py
+++ src/checkout/basket_handler.py
@@ -38,20 +38,21 @@ def get_total_price(sku_dict: dict) -> int:
 
     total_price = 0
 
-    for key, value in list(sku_dict.items()):
-        print(key)
-        if key not in price_table:
-            raise ValueError("Error: SKU not in price table")
+    # for key, value in list(sku_dict.items()):
+    #     print(key)
+    #     if key not in price_table:
+    #         raise ValueError("Error: SKU not in price table")
 
-        while sku_dict[key] > 0:
-            if ("special offer" in price_table[key] and
-               sku_dict[key] > price_table[key]["special_offer"]["quantity"]):
-                # if special offer exists and quantity exceeds required
-                total_price += price_table[key]["special_offer"]["price"]
-                sku_dict[key] = (sku_dict[key] -
-                                 price_table[key]["special_offer"]["quantity"])
-            else:
-                total_price += price_table[key]["price"]
-                sku_dict[key] = sku_dict[key] - 1
+    #     while sku_dict[key] > 0:
+    #         if ("special offer" in price_table[key] and
+    #            sku_dict[key] > price_table[key]["special_offer"]["quantity"]):
+    #             # if special offer exists and quantity exceeds required
+    #             total_price += price_table[key]["special_offer"]["price"]
+    #             sku_dict[key] = (sku_dict[key] -
+    #                              price_table[key]["special_offer"]["quantity"])
+    #         else:
+    #             total_price += price_table[key]["price"]
+    #             sku_dict[key] = sku_dict[key] - 1
 
     return total_price
+
