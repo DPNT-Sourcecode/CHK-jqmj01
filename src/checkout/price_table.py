912b709eb80bc10c4550eef50a2e2659bf73be66
sbx_user1051 <sbx_user1051@169.254.65.13>
2022-07-17 19:59:39 +0000

    Sun Jul 17 19:59:39 UTC 2022

--- src/checkout/price_table.py
+++ src/checkout/price_table.py
@@ -5,7 +5,27 @@ price_table = {
         "C": 20,
         "D": 15,
         "E": 40,
-        "F": 10
+        "F": 10,
+        "G": 20,
+        "H": 10,
+        "I": 35,
+        "J": 60,
+        "K": 80,
+        "L": 90,
+        "M": 15,
+        "N": 40,
+        "O": 10,
+        "P": 50,
+        "Q": 30,
+        "R": 50,
+        "S": 30,
+        "T": 20,
+        "U": 40,
+        "V": 50,
+        "W": 20,
+        "X": 90,
+        "Y": 10,
+        "Z": 50
     },
     "offers": [  # in order of value to the customer
         {
@@ -16,6 +36,29 @@ price_table = {
                 "details": {
                     "sku": "B",
                     "quantity": 1
+                }
+            }
+        },
+        {
+            "sku": "N",
+            "quantity": 3,
+            "offer": {
+                "type": "freebie",
+                "details": {
+                    "sku": "M",
+                    "quantity": 1
+                }
+            }
+        },
+        {
+            "sku": "F",
+            "quantity": 2,
+            "offer": {
+                "type": "bogof",
+                "details": {
+                    "sku": "F",
+                    "quantity": 1,
+                    "total_items_needed": 3
                 }
             }
         },
@@ -44,18 +87,47 @@ price_table = {
             }
         },
         {
-            "sku": "F",
+            "sku": "H",
+            "quantity": 10,
+            "offer": {
+                "type": "reduced_price",
+                "details": 80
+            }
+        },
+        {
+            "sku": "H",
+            "quantity": 5,
+            "offer": {
+                "type": "reduced_price",
+                "details": 45
+            }
+        },
+        {
+            "sku": "K",
             "quantity": 2,
             "offer": {
-                "type": "bogof",
-                "details": {
-                    "sku": "F",
-                    "quantity": 1,
-                    "total_items_needed": 3
-                }
+                "type": "reduced_price",
+                "details": 150
             }
-        }
+        },
+        {
+            "sku": "P",
+            "quantity": 5,
+            "offer": {
+                "type": "reduced_price",
+                "details": 200
+            }
+        },
+        {
+            "sku": "Q",
+            "quantity": 3,
+            "offer": {
+                "type": "reduced_price",
+                "details": 80
+            }
+        },
     ]
 }
+
 
 
