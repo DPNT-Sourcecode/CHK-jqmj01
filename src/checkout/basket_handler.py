879dd57512825101446ea0d1daee19f132852541
sbx_user1051 <sbx_user1051@169.254.1.109>
2022-07-17 16:32:20 +0000

    Sun Jul 17 16:32:20 UTC 2022

--- src/checkout/basket_handler.py
+++ src/checkout/basket_handler.py
@@ -1,6 +1,6 @@
 from src.checkout.price_table import price_table
 
-def get_sku_dict(skus: str) -> int:
+def get_sku_dict(skus: str) -> dict:
     '''Get the total number of each SKU in the basket
     
     Args: 
@@ -8,7 +8,7 @@ def get_sku_dict(skus: str) -> int:
         (assumed to be a list of the SKUS like "AAABBC" for now)
 
     Returns: 
-        int: the total checkout value of the items
+        dict: a dict containing the quantity of each SKU in the string
     '''
 
     # convert to list
@@ -24,6 +24,28 @@ def get_sku_dict(skus: str) -> int:
 
     return sku_dict
     
-
+def get_total_price(sku_dict: dict) -> int:
+    '''Get the total price of the SKUS in the input dict
     
+    Args: 
+        skus (dict): a dict containing the number of each SKU in the basket
+
+    Returns: 
+        int: the total price of all the SKUs in the basket
+    '''
+
+    total_price = 0
+
+    for sku in sku_dict:
+        while sku_dict[sku] > 0: 
+            if "special offer" in price_table[sku] and sku_dict[sku] > price_table[sku]["special_offer"]["quantity"]:
+                # if special offer exists and quantity exceeds required for offer
+                total_price += price_table[sku]["special_offer"]["price"]
+                sku_dict[sku] = sku_dict[sku] - price_table[sku]["special_offer"]["quantity"]
+            else:
+                total_price += price_table[sku]["price"]
+                sku_dict[sku] = sku_dict[sku] - 1
+    
+    return total_price
+
 
