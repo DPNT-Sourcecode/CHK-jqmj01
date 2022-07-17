9f1f1bff4e0151b551edd7261fc8a59a01f6b57f
sbx_user1051 <sbx_user1051@169.254.1.109>
2022-07-17 16:08:31 +0000

    Sun Jul 17 16:08:31 UTC 2022

--- src/checkout/round_one.py
+++ src/checkout/round_one.py
@@ -1,3 +1,24 @@
 from price_table import price_table
 
-print(price_table)
+def get_total_price(skus: str) -> int:
+    '''Get the total price from the price table of multiple items
+    
+    Args: 
+        skus (string): a string containing the SKUS of all the products in the basket 
+        (assumed to be a list of the SKUS like "AAABBC" for now)
+
+    Returns: 
+        int: the total checkout value of the items
+    '''
+
+    sku_list = skus.split("")
+
+    # count total of each sku in basket
+    sku_dict = {}
+    for sku in sku_list:
+        if sku not in sku_dict:
+            sku_dict[sku] = 1
+        else:
+            sku_dict[sku] = sku_dict[sku] + 1
+
+    
