35f1f66eeb75d2de4f5fdda0267927bfee50ca97
sbx_user1051 <sbx_user1051@169.254.1.109>
2022-07-17 16:47:20 +0000

    Sun Jul 17 16:47:20 UTC 2022

--- src/lib/solutions/CHK/checkout_solution.py
+++ src/lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,18 @@
-from src.checkout.basket_handler import get_sku_dict, get_total_price
+from checkout.basket_handler import get_sku_dict, get_total_price
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+    '''Get the total price from a list of SKUs
+    
+    Args: 
+        skus (string): the skus in the basket 
+
+    Returns: 
+        int: the total price of all the skus
+    '''
+
+    skus_dict = get_sku_dict(skus)
+    total_price = get_total_price(skus_dict)
+    return total_price
+
