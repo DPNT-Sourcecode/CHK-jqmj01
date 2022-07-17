73ed7a24b3b4293787d26c24f7e39a17e609aad6
sbx_user1051 <sbx_user1051@169.254.1.109>
2022-07-17 16:14:31 +0000

    Sun Jul 17 16:14:31 UTC 2022

---
+++ test/solution_tests/CHK/test_basket_handler.py
@@ -0,0 +1,16 @@
+from checkout.basket_handler import get_sku_dict
+import pytest
+
+class TestBasketHandler():
+    def test_get_sku_dict(self):
+        """
+        GIVEN the get_sku_dict method
+        WHEN two positive numbers between 0 and 100 are passed
+        THEN the output is the sum of those two numbers
+        """
+        assert get_sku_dict("AABBBCCD") == {
+            "A": 2,
+            "B": 3,
+            "C": 2,
+            "D": 1
+        }
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -5,7 +5,7 @@ import pytest
 class TestSum():
     def test_sum(self):
         """
-        GIVEN the sum_two_numbers fuction
+        GIVEN the sum_two_numbers method
         WHEN two positive numbers between 0 and 100 are passed
         THEN the output is the sum of those two numbers
         """
@@ -13,7 +13,7 @@ class TestSum():
 
     def test_sum_raises_exception(self):
         """
-        GIVEN the sum_two_numbers fuction
+        GIVEN the sum_two_numbers method
         WHEN one of the input numbers is outside the 0-100 range
         THEN a ValueError exception is raised
         """
@@ -27,3 +27,4 @@ class TestSum():
             res = sum_solution.compute(2, -100)
         
         assert "num2 must be in the range 0-100" in str(e)
+
