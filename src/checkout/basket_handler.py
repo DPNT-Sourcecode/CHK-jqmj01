e7aa98ee4fc1de5345ef8ef11bbe543b86e5882d
sbx_user1051 <sbx_user1051@169.254.1.109>
2022-07-17 17:50:05 +0000

    Sun Jul 17 17:50:05 UTC 2022

--- src/checkout/basket_handler.py
+++ src/checkout/basket_handler.py
@@ -15,10 +15,10 @@ def get_sku_dict(skus: str) -> dict:
     # convert to list
     sku_list = [sku.upper() for sku in skus]
 
-    # check for invalid skus
-    for sku in sku_list:
-        if sku not in price_table:
-            raise ValueError("Error: SKU not in price table")
+    # # check for invalid skus
+    # for sku in sku_list:
+    #     if sku not in price_table:
+    #         raise ValueError("Error: SKU not in price table")
 
     # count total of each sku in basket
     sku_dict = {}
@@ -44,15 +44,17 @@ def get_total_price(sku_dict: dict) -> int:
     total_price = 0
 
     for key, value in list(sku_dict.items()):
-        while sku_dict[key] > 0:
-            if ("special offer" in price_table[key] and
-               sku_dict[key] > price_table[key]["special_offer"]["quantity"]):
-                # if special offer exists and quantity exceeds required
-                total_price += price_table[key]["special_offer"]["price"]
-                sku_dict[key] = (sku_dict[key] -
-                                 price_table[key]["special_offer"]["quantity"])
-            else:
-                total_price += price_table[key]["price"]
-                sku_dict[key] = sku_dict[key] - 1
+        if key in price_table:
+            while sku_dict[key] > 0:
+                if ("special offer" in price_table[key] and
+                sku_dict[key] > price_table[key]["special_offer"]["quantity"]):
+                    # if special offer exists and quantity exceeds required
+                    total_price += price_table[key]["special_offer"]["price"]
+                    sku_dict[key] = (sku_dict[key] -
+                                    price_table[key]["special_offer"]["quantity"])
+                else:
+                    total_price += price_table[key]["price"]
+                    sku_dict[key] = sku_dict[key] - 1
 
     return total_price
+
